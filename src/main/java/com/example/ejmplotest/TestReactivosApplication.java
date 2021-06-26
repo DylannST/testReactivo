2fd108c68bc2297378c636429c5147d5d0455f69
DylannS <dguarnizoserrato@gmail.com>
2021-06-26 11:36:37 -0500

    se agrega la practica de test para entender mejor

---
+++ src/main/java/com/example/ejmplotest/TestReactivosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejmplotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReactivosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestReactivosApplication.class, args);
+    }
+
+}
